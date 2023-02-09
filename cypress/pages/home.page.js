976f21127c05fd9aa1c64b1fc74ada7f0d53ac2a
qafromla <diana.la777@address.com>
2023-02-08 21:34:24 -0800

    fixing

--- cypress/pages/home.page.js
+++ cypress/pages/home.page.js
@@ -2,6 +2,9 @@ class homePage {
 	get bannerTitle() {
 		return cy.get('.logo-font')
 	}
+	get signUpButton() {
+		return cy.get('[href="#/register"]')
+	}
 
 	visit() {
 		cy.visit('https://qafromla.herokuapp.com/')
