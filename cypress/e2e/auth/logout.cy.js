46cadc7e56b936715342e93a84a533cf567be689
qafromla <diana.la777@address.com>
2023-02-14 15:39:13 -0800

    new test login

--- cypress/e2e/auth/logout.cy.js
+++
@@ -1,5 +0,0 @@
-describe('Auth | Logout', () => {
-  it('passes', () => {
-    cy.visit('https://qafromla.herokuapp.com/')
-  })
-})
--- cypress/e2e/auth/registration.cy.js
+++
@@ -1,5 +0,0 @@
-describe('Auth | Registration', () => {
-  it('passes', () => {
-    cy.visit('https://qafromla.herokuapp.com/')
-  })
-})
