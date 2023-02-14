329044126a3c280ceb776a59ba1d8b503122951d
qafromla <diana.la777@address.com>
2023-02-14 15:22:20 -0800

    add new test user special characters

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -23,4 +23,10 @@ describe('Registration', () => {
 		RegisatrationPage.signUpNewAccount(uniqUsername, email, uniqPassword)
 		cy.contains('Email already exists.. try logging in').should('be.visible')
 	})
+
+	it('should not be able to create an username with special characters, QALA-24', () => {
+		homePage.signUpButton.click()
+		RegisatrationPage.signUpNewAccount('Baby bus', uniqEmail, uniqPassword)
+		cy.contains('Username cannot contain empty characters').should('be.visible')
+	})
 })
