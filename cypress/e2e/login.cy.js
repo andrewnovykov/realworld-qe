80dd61e118bacb29822138f30e1eaf9505716fab
qafromla <diana.la777@address.com>
2023-02-15 19:59:50 -0800

    add new test case QALA-7

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,6 @@
-import RegisatrationPage from '../pages/registration.page'
+import LoginPage from '../pages/login.page'
 import homePage from '../pages/home.page'
-
-describe('Registration', () => {
-	const uniqueId = Date.now()
-	const uniqUsername = `uName${uniqueId}`
-	const uniqEmail = `email${uniqueId}@gamil.com`
-	const uniqPassword = `password${uniqueId}`
+describe('Login', () => {
 	const username = 'Babybus'
 	const email = 'babybus@gmail.com'
 	const password = 'QWERTY12345'
@@ -13,14 +8,8 @@ describe('Registration', () => {
 		homePage.visit()
 	})
 	it('should be able to login with existing account, QALA-7', () => {
-		homePage.signUpButton.click()
-		RegisatrationPage.signUpNewAccount(uniqUsername, uniqEmail, uniqPassword)
-		cy.contains(uniqUsername).should('be.visible')
-	})
-
-	it('should not be able sign up with  an already registered email address, QALA-6', () => {
-		homePage.signUpButton.click()
-		RegisatrationPage.signUpNewAccount(uniqUsername, email, uniqPassword)
-		cy.contains('Email already exists.. try logging in').should('be.visible')
+		homePage.logInButton.click()
+		LoginPage.logIn(email, password)
+		cy.contains(username).should('be.visible')
 	})
 })
