1d4b659ccc93bd765c0b8f86ac2807bb32f29ef0
qafromla <diana.la777@address.com>
2023-03-06 20:58:11 -0800

    config

--- cypress/e2e/api/create-article.cy.js
+++ cypress/e2e/api/create-article.cy.js
@@ -1,7 +1,36 @@
 describe('POST /api/articles', () => {
-	it('Create Article', () => {
-		cy.loginAsMainUser()
+	let myToken
+	it('should be able to create an article with with Valid Inputs, QALA-25', () => {
+		cy.loginAsMainUser().then((token) => {
+			expect(token).to.exist
+			// Cypress.env('AUTH_TOKEN', token)
+			console.log(`TOKEN : ->>> ${token}`)
+			myToken = token
+		})
 		// Perform some action that requires the user to be logged in
 		//Create Article
+		const uniqueId = Date.now()
+		const uniqueTitle = `API${uniqueId}`
+		const createArticle = {
+			article: {
+				title: uniqueTitle,
+				description: 'Rising Need For API Testing',
+				body: 'An API abstracts the implementation layer',
+				tagList: ['API'],
+			},
+		}
+		cy.request({
+			method: 'POST',
+			url: '/api/articles',
+			headers: {
+				Authorization: myToken,
+			},
+			body: createArticle,
+			failOnStatusCode: false,
+			gzip: true,
+		}).then((response) => {
+			expect(response.status).to.eq(201)
+			expect(response.body.article).to.have.property('article')
+		})
 	})
 })
--- cypress/support/login.js
+++ cypress/support/login.js
@@ -37,3 +37,27 @@ Cypress.Commands.add('login', (email, password) => {
 		.its('body.user.token')
 		.should('exist')
 })
+
+Cypress.Commands.add('loginAsMainUserToken', () => {
+	cy.fixture('user.data').then((userData) => {
+		const loginData = {
+			user: {
+				email: userData.email,
+				password: userData.password,
+			},
+		}
+
+		cy.request({
+			method: 'POST',
+			url: '/api/users/login',
+			body: loginData,
+			failOnStatusCode: false,
+			gzip: true,
+		})
+			.its('body.user.token')
+			.then((token) => {
+				// Return the token from the callback
+				return token
+			})
+	})
+})
